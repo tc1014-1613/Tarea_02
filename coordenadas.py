bc81d60df138082101cd7f520a3a5a32860655f9
Roberto Martínez Román <rmroman@itesm.mx>
2016-08-19 10:49:18 -0500

    Update coordenadas.py

--- coordenadas.py
+++ coordenadas.py
@@ -3,6 +3,7 @@
 # Autor: tuNombreCompleto, tuMatricula
 # Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
 
+from math import atan2, pi
 # A partir de aqui escribe tu programa
 
-from math import atan2, pi
+

6be0c9e076fb72d3579597b34230c9d5e7807b93
Edgarlv <a01371424@itesm.mx>
2016-08-21 13:07:23 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,6 +1,14 @@
 #encoding: UTF-8
+# Autor: Edgar Eduardo Alvarado Duran.   A01371424
+# Ejercicio 1
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
-
-# A partir de aqui escribe tu programa
+nombre= "Edgar Eduardo Alvarado Durn"
+print (nombre)
+matricula= "A01371424"
+print (matricula)
+carrera= "ISDR"
+print (carrera)
+descrip= "Estudio ISDR porque siempre me gusto programar y construir a la vez" 
+print (descrip)
+descrip2= "Estudié en la prepa TEC CEM"
+print (descrip2)

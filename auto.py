1d278ac7fccc7290c1519f4917549b3c3e926bff
KarlaAlcantara <A01373164@itesm.mx>
2016-08-21 17:31:11 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,8 +1,19 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
+# Autor: Karla Valeria Alcántara Duarte A01373164 
+# Descripcion:Calcular la distancia y el tiempo que tarda un auto de acuerdo a la velocidad que lleva.
 
-# A partir de aqui escribe tu programa
+velocidad = int(input("Introduce la distancia en km/hr"))
+tiempo1 = 6
+tiempo2 = 10
+# v = d/t
+distancia1 = velocidad*tiempo1
+distancia2 = velocidad*tiempo2
+distancia = 500
+tiempo = distancia/velocidad
+
+print("La distancia que recorre en 6 hrs:",distancia1,"kilometros")
+print("La distancia que recorre en 10 hrs:",distancia2,"kilometros")
+print("El tiempo en el que recoree 500 km:",tiempo,"horas")
 
 

6a924e065fedafb421b13077ad5613bbe1c689fa
Edgarlv <a01371424@itesm.mx>
2016-08-21 13:57:53 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,8 +1,11 @@
 #encoding: UTF-8
+# Autor: Edgar Eduardo Alvarado Duran.      A01371424
+# Problema 2
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
-
-# A partir de aqui escribe tu programa
-
-
+velocidad= int(input("Velocidad promedio en km/h"))
+res1= velocidad*6
+res2= velocidad*10
+tiempo= 500/velocidad
+print ("La distancia que recorre en 6 horas es de: ", res1, " km")
+print ("La distancia que recorre en 10 horas es de: ", res2, " km")
+print ("El tiempo que tarda en recorrer 500 km es de: ", tiempo, " horas")

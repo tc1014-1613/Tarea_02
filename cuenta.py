6edefbe336f104ddbdaf387e8aec89b8bacd33d9
Edgarlv <a01371424@itesm.mx>
2016-08-23 18:23:31 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -6,7 +6,7 @@ cuenta= int(input("Total de su comida"))
 propina= cuenta*.15
 IVA= cuenta*.16
 total= cuenta+propina+IVA
-print ("Subtotal de la comida es de ", cuenta)
-print ("Propina ", propina)
-print ("IVA ", IVA)
-print ("Total de su cuenta es de ", total)
+print ("Subtotal de la comida es de $", cuenta)
+print ("Propina $", propina)
+print ("IVA $", IVA)
+print ("Total de su cuenta es de $", total)

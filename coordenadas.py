6ef8cb17cd09b67c6f88fed0f10ab3a5fdd543d6
Adrian Tellez <A01372746@itesm.mx>
2016-08-23 16:45:28 -0500

    Add files via upload

--- coordenadas.py
+++ coordenadas.py
@@ -1,10 +1,20 @@
 #encoding: UTF-8
+# Autor: Adrián E. Téllez López
+# Sacar el porcentaje de hombres y mujeres en una clase
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
-
-from math import atan2, pi
+import math
 
 # A partir de aqui escribe tu programa
+
+x = int(input("Cual es el valor de x"))
+y = int(input("Cual es el valor de y"))
+
+z = (x * x) + (y * y)
+magnitud = math.sqrt(z)
+grad = math.atan2(y,x)
+grad = math.degrees(grad)
+
+print ("Magnitud:", magnitud)
+print ("Angulo:", grad, "°")
 
 
--- porcentajes.py
+++ porcentajes.py
@@ -1,6 +1,16 @@
 #encoding: UTF-8
-
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
+# Autor: Adrián E. Téllez López
+# Sacar el porcentaje de hombres y mujeres en una clase
 
 # A partir de aqui escribe tu programa
+
+hombres = int(input("Cuantos hombres hay inscritos en la clase"))
+mujeres = int(input("Cuantas mujeres hay inscritos en la clase"))
+
+total = hombres + mujeres
+Porcentajeh = (hombres * 100)/total
+Porcentajem = (mujeres * 100)/total
+
+print ("Hay", total, "personas en la clase")
+print ("El porcentaje de hombres en la clase es:", Porcentajeh, "%")
+print ("El porcentaje de mujeres en la clase es:", Porcentajem, "%")

470b88457fdc1aed36843ac2251d0adcfffdcbcb
Allan Sánchez <all4nxp@icloud.com>
2016-08-24 15:10:27 -0500

    Add files via upload

--- cuenta.py
+++ cuenta.py
@@ -1,6 +1,14 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
+# Autor: Sánchez Iparrazar Allan A01379951
+# Descripión: Calcula el total a pagar, iva y propina de una comida
 
 # A partir de aqui escribe tu programa
+total = int(input("Introduce el costo de la comida"))
+iva = total*0.16
+propina = total*0.15
+Total=total+iva+propina
+print("Total de la comida: $",total)
+print("IVA: $",iva)
+print("Propina: $",propina)
+print("Total a pagar: $",Total)
--- porcentajes.py
+++ porcentajes.py
@@ -1,6 +1,16 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
+# Autor: Sánchez Iparrazar Allan A01379951
+# Descripcion: Porcentaje de hombres y mujeres que hay en una clase
 
 # A partir de aqui escribe tu programa
+NH = int(input("¿Cuántos hombres hay en la clase?"))
+NM = int(input("¿Cuántas mujeres hay en la clase?"))
+
+NT=NH+NM
+PH=(NH/NT)*100
+PM=(NM/NT)*100
+
+print("Número total de alumnos:",NT)
+print("Porcentaje de hombres:", PH,"%")
+print("Porcentaje de mujeres:", PM,"%")

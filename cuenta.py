540e2eb5dff3c5792411100532b51ce818e57023
Manuel Alejandro Bracho / A01378897 <brachojr_97@hotmail.com>
2016-08-23 19:29:42 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,6 +1,16 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
+# Autor: Manuel Alejandro Bracho Mendoza, A01378897
+# Descripcion: Este programa calcula el total de una comida con el porsentaje de propina e iva.
 
 # A partir de aqui escribe tu programa
+
+subtotal=int(input("Porfavor, ingrese su sibtotal a pagar"
+propina=subtotal * 1.15 -subtotal
+iva=subtotal * 1.16 - subtotal
+total=subtotal+iva+propina
+print("el subtotal es",subtotal)
+print("la propina es de",propina)
+print("la iva es de",iva)
+print("el total a pagar es de", total)
+

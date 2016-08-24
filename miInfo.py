6a2513753910e2c1caa6c50f3d5a066f91a10c59
Ian González <a01373302@itesm.mx>
2016-08-23 21:07:44 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,6 +1,16 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
+# Autor: Ian González Pámanes
+# Descripcion: Imprime mi información.
 
 # A partir de aqui escribe tu programa
+
+def main():
+  print("Ian González Pámanes")
+  print("A01373302")
+  print("ISC")
+  print("Me gustan los videojuegos y el anime")
+  print("Estudie el Bachillerato Internacional")
+  print("Algun dia me gustaria trabajar en Blizzard Entretainment")
+  
+main()

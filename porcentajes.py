55a57db83d01ee22f471c7b60acb23f925f1c3db
Edgarlv <a01371424@itesm.mx>
2016-08-23 20:59:00 -0500

    Update porcentajes.py

--- porcentajes.py
+++ porcentajes.py
@@ -1,6 +1,14 @@
 #encoding: UTF-8
+# Autor: Edgar Eduardo Alvarado Duran,     A01371424
+# Problema 4
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
-
-# A partir de aqui escribe tu programa
+hombres= int(input("¿Cuantos hombres estan inscritos?"))
+mujeres= int(input("¿Cuantas mujeres estan inscritas?"))
+inscritos= hombres+mujeres
+h= hombres*100
+porcientoh= h/inscritos
+m= mujeres*100
+porcientom= m/inscritos
+print ("El total de inscritos son ", inscritos)
+print ("El porcentaje de hombres es de ", porcientoh, "%")
+print ("El porcentaje de mujeres es de ", porcientom, "%")

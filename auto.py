9450cfd69fd76d6acd724c47d8c5999cc495b34c
Manuel Alejandro Bracho / A01378897 <brachojr_97@hotmail.com>
2016-08-23 19:14:21 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,9 +1,18 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
+# Autor: Manuel Alejandro Bracho Mendoza, A01378897
+# Descripcion: Calcula velocidad en 6 y 10 hrs, así como el tiempo que tarda en recorrer 500km
 
 # A partir de aqui escribe tu programa
+def main () : 
+  velocidad=int(input("ingresa la velocidad en la que viaja el auto")
+  velocidadEnSeisHoras=velocidad*6
+  velocidadEnDiezHoras=velocidad*10
+  tiempoQueTarda500=500/velocidad
+  print("la velocidad en seis horas es de", velocidadEnSeisHoras "km/h")
+  print("la velocidad en diez horas es de", velocidadEnDiezHoras "km/h")
+  print("El tiempo que tarda en recorrer 500 km es de", tiempoQueTarda500 "hrs")
+  
+main ()
 
-
-# Solucion
+# Solucion ?

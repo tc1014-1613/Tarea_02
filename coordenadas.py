0dc7c42a725f29147c0f6518cd3b5beb323565e4
MarinaHaro <A01373471@itesm.mx>
2016-08-24 00:48:25 -0500

    Tarea completa(:
    Marina Haro A01373471

--- coordenadas.py
+++ coordenadas.py
@@ -1,10 +1,22 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
+# Autor: Marina Itzel Haro Hernández, A01373471
+# Descripcion: Convierte coordenadas cartesianas a polares
 
-from math import atan2, pi
+from math import atan2
+import math
 
 # A partir de aqui escribe tu programa
+
+x = int(input("Coloca el valor de x"))
+y = int(input("Coloca el valor de y"))
+
+r = ((x**2 + y**2)**(1/2))
+angulo = math.atan2(y,x)
+angulo = math.degrees(angulo)
+
+print ("El valor de r es", r)
+print ("El valor del angulo es", angulo, "grados")
+
 
 
--- porcentajes.py
+++ porcentajes.py
@@ -1,6 +1,19 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
+# Autor: Marina Itzel Haro Hernández, A01373471
+# Descripción: Calcula el total de alumnos y el porcentaje de hombres y mujeres de acuerdo a la cantidad de hombres y mujeres.
 
 # A partir de aqui escribe tu programa
+
+def main():
+    mujeres = float(input("Número de mujeres inscritas en clase"))
+    hombres = float(input("Número de hombres inscritos en clase"))
+    totalDeAlumnos = mujeres + hombres
+    porcentajeMujeres = (mujeres*100)/totalDeAlumnos
+    porcentajeHombres = (hombres*100)/totalDeAlumnos
+    
+    print("El total de alumnos inscritos en clase es", totalDeAlumnos)
+    print("El porcentaje de mujeres en clase es", porcentajeMujeres, "%")
+    print("El porcentaje de hombres en clase es", porcentajeHombres, "%")
+    
+main()

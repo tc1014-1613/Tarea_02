fba380d05bf0dde73b39d63240584995820124f7
María Angélica Hernández Parada <mariaangelic_132@hotmail.com>
2016-08-23 21:27:48 -0500

    Tarea 2
    Tarea finalizada

--- auto.py
+++ auto.py
@@ -1,8 +1,22 @@
 #encoding: UTF-8
-
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
-
+# Autor: Mara Anglica Hernandez Parada, A01169796
+# Descripcion: calcular la distancia, velocidad o tiempo de acuerdo a lo que nos dan 
 # A partir de aqui escribe tu programa
 
+tiempo=6
+velocidad=int(input("Dime a que velocidad iba el auto en km/h"))
+distancia=tiempo*velocidad
 
+print ("la distancia que recorrio fue", distancia, "km")
+
+tiempo=10
+velocidad=int(input("Dime a que velocidad iba el auto en km/h"))
+distancia=tiempo*velocidad
+
+print ("la distancia que recorrio fue", distancia, "km")
+
+distancia=500
+velocidad=int(input("Dime a que velocidad iba el auto en km/h"))
+tiempo=distancia/velocidad
+
+print ("el tiempo que tardo fue de", tiempo, "hrs")

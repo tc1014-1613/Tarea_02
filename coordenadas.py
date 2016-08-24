efa2e9709622252581ba805f5538de1ef937302f
Manuel Alejandro Bracho / A01378897 <brachojr_97@hotmail.com>
2016-08-23 20:50:23 -0500

    Update coordenadas.py

--- coordenadas.py
+++ coordenadas.py
@@ -1,10 +1,22 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
+# Autor: Manuel Alejandro Bracho Mendoza, A01378897
+# Descripcion: calcula el angulo y la magnitud dadas X y Y .
 
-from math import atan2, pi
+from math import atan2, pi , sqrt
 
 # A partir de aqui escribe tu programa
+x=int(input("ingrese el valor de x")
+y=int(input("ingrese el valor de y")
+
+magnitud=math.sqrt((x*x)+(y*y))
+anguloAtan=math.atan2(y,x)
+anguloFinal=math.degrees(grados)
+
+print("la magnitud es de", magnitud)
+print("el angulo es de", anguloFinal "grados")
+
+
+
 
 

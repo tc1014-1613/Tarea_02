3038699f1366338ddace35a96db9c78ea1a37110
Ian González <a01373302@itesm.mx>
2016-08-23 20:54:08 -0500

    Update coordenadas.py

--- coordenadas.py
+++ coordenadas.py
@@ -7,4 +7,13 @@ from math import atan2, pi
 
 # A partir de aqui escribe tu programa
 
+def main():
+  
+  x = int(input("introduzca coordenada x"))
+  y = int(input("introduzca coordenada y"))
+  
+  R = (x**2 + y**2)**.5
+  angulo = atan2(y,x)
+  angulo = angulo*180/3.1416
 
+print ("El vector es:", r, angulo)

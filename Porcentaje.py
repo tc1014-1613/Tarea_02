95dc27ce427685475eed9f9441eff48abe172722
A013740009 <hd170998@hotmail.com>
2016-08-24 21:01:35 -0500

    Add files via upload

---
+++ Porcentaje.py
@@ -0,0 +1,12 @@
+#Hector David Hernandez Rodriguez
+#A01374009
+#
+m=int(input("Numero de mujeres"))
+h=int(input("Numero de hombres"))
+c=m+h
+Pm=(m/c)*100
+Ph=(h/c)*100
+Pm1=round(Pm,(0))
+PM2=round(Ph,(0))
+print("el porcentaje de mujeres en el salon es de ",Pm1,"%")
+print("el porcentaje de hombres en el salon es de ",Pm2,"%")
--- auto.py
+++ auto.py
@@ -1,9 +1,10 @@
-#encoding: UTF-8
-
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
-
-# A partir de aqui escribe tu programa
-
-
-# Solucion
+#Hector David Hernandez Rodriguez
+#A01374009
+#Auto
+v=int(input("Introduzca velocidad en km/h"))
+d6=v*6
+d10=v*10
+t=500/v
+print("la distancia recorrida en 6 hrs es ",d6)
+print("la distancia recorrida en 10 hrs es ",d10)
+print("recorreras 500 km aproximadamente en",t,"horas")

dc56ae505b4942847a52b92dec1ff81e758cfa7f
FernandaRodriguez <A01371649@itesm.mx>
2016-08-24 19:18:18 -0500

    Add files via upload
    falto ejercicio 5

--- auto.py
+++ auto.py
@@ -1,8 +1,17 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
+# Autor: Maria Fernanda Rodriguez Hernandes, A01371649
+# Descripcion: la distancia en 6 y 10 hrs en m y el tiempo que requiere para recorrer 500km en s.
 
-# A partir de aqui escribe tu programa
+velocidad = int(input("a que velocidad iba el auto en m/s"))
 
+distancia_6 = velocidad * 360
+
+distancia_10 = velocidad * 600
+
+tiempo = velocidad / 500000
+
+print("la distancia que recorrio en 6 hrs fue de",distancia_6,"m")
+print("la distancia que recorrio en 10 hrs fue de", distancia_10,"m")
+print("el tiempo que tardo en recorrer 500 km fue de",tiempo,"s")
 
--- cuenta.py
+++ cuenta.py
@@ -1,6 +1,17 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estas resolviendo.
+# Autor: Maria Fernanda Rodriguez Hernandes, A01371649
+# Descripcion: este es un programa qe calcula el iva la propina y el total de la cuenta de un restaurante .
 
-# A partir de aqui escribe tu programa
+numero = int(input("Cuntos platillos ordeno? "))
+suma = 0
+for i in range(1, numero + 1):
+    platillo = int(input("Escriba el costo del " + str(i) + "platillo "))
+    suma = suma + platillo
+    
+    iva = suma*.16
+    propina = suma*.15
+    total= suma +iva+propina
+
+print("el subtotales:",suma,"el iva es:",iva,"la propina es:",propina,"el total es",total)    
+                    
